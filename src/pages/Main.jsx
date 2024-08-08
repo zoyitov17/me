bedc5bbece3183046b73d316154adc88ca9d97a6
Adhamjon Zoyitov <adhamjonzokhidov@gmail.com>
2024-08-08 15:25:30 +0500

    all done

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -9,7 +9,7 @@ function Main() {
           <h1 className="text-white font-sans font-bold text-5xl md:text-4xl sm:text-3xl xs:text-2xl custom-500:text-[26px] custom-400:text-[26px]">
             Adhamjon Zoyitov
           </h1>
-          <div className="flex items-center justify-center w-full h-[90px] mt-14 border border-white md:mt-10 sm:mt-8 custom-500:mt-6 custom-400:mt-4">
+          <div className="flex items-center justify-center w-full h-[90px] mt-16 border border-white md:mt-10 sm:mt-8 custom-500:mt-6 custom-400:mt-4">
             <Link
               to="/aboutme"
               className="text-white font-sans font-semibold text-3xl hover:text-yellow-300 sm:text-2xl xs:text-xl custom-500:text-lg custom-400:text-md"
